5bf1835160a2c51cd62e696547097e502a374c88
Horen Star <51239921+TaperingBirch@users.noreply.github.com>
2023-04-03 20:53:59 -0600

    Tweaks

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -5,4 +5,8 @@ var character: Node3D
 
 # A dictionary containing all of the nodes that make up the shrunk character 
 # node
-var cosmetics_enabled: Dictionary # {"Body": "Body02", "Hair": "Hair01", etc.}
+var cosmetics_enabled: Dictionary = {
+	"Body": "Body01", 
+	"Hair": "Hair01", 
+	"Pants": "Pants01"
+}
